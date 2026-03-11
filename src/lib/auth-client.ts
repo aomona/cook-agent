ca928b82ed5dc8c911057fd9a4e4356943ea7780
aomona <yuka41623@gmail.com>
2026-03-11 16:36:21 +0900

    feat(auth): add github oauth integration

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,16 @@
+import { createAuthClient } from 'better-auth/client';
+
+const authClient = createAuthClient();
+
+export const signIn = async () => {
+	const data = await authClient.signIn.social({
+		provider: 'github',
+	});
+	return data;
+};
+
+export const signOut = async () => {
+	await authClient.signOut();
+};
+
+export const { useSession } = authClient;
